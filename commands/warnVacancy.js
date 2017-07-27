c45c0d9f400e6c2a2935ab8ebcb0d8e01642b261
Rafael Fakhreev <fahreevrafel@yandex.ru>
2017-07-27 23:48:21 +0300

    Fix warn message

--- commands/warnVacancy.js
+++ commands/warnVacancy.js
@@ -6,12 +6,10 @@ const keywords = new Set(['формат']);
 async function warnVacancy(msg) {
   console.log('warnVacancy');
   const replyText = 'Надо отредактировать по формату — тогда вакансия попадёт в канал ' +
-    '[@javascript_jobs_feed](https://t.me/javascript_jobs_feed). ' +
-    'Иначе её удалят. ' +
-    '[Смотри правила](http://telegra.ph/Pravila-oformleniya-vakansij-03-28)';
+    '@javascript_jobs_feed. Иначе её удалят.\n' +
+    'Смотри правила: http://telegra.ph/Pravila-oformleniya-vakansij-03-28';
   await bot.sendMessage(msg.chat.id, replyText, {
-    reply_to_message_id: msg.reply_to_message.message_id,
-    parse_mode: 'Markdown'
+    reply_to_message_id: msg.reply_to_message.message_id
   })
 }
 
