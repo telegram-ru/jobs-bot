388acd56054904dc9ce709c4bb7a7521f6d84984
Evgeny Rodionov <git@erodionov.ru>
2017-08-03 01:42:56 +0300

    chore: add simple http server for zeit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const http = require("http");
 const { bot, channelId, setChannelId } = require("./init");
 const { admins, updateAdmins } = require("./admins");
 const exec = require("./commands/index");
@@ -8,3 +9,10 @@ setChannelId()
   .then(() => {
     bot.on("message", msg => exec(msg));
   });
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end();
+});
+
+server.listen(3000);
