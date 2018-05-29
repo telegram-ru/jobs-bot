dcb6a185fd73a379ed9da3497752ce4107fde1cc
Evgeny Rodionov <git@erodionov.ru>
2018-05-29 10:35:34 +0300

    chore: add info about bot & state of polling

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const debug = require('debug')('jobs-bot');
+const debug = require('debug')('jobs-bot:init');
 const http = require('http');
 const bot = require('./bot');
 const { setAllAdmins, admins } = require('./admins');
@@ -6,9 +6,13 @@ const exec = require('./commands/index');
 
 // set admins && set handlers messages
 Promise.all(setAllAdmins())
+  .then(async () => debug('bot', await bot.getMe()))
   .then(() => bot.on('message', msg => exec(msg)))
   .then(() => debug('admins', admins))
   .catch((...args) => debug('error @ init', args));
+
+// check is polling
+setInterval(() => debug('isPolling', bot.isPolling()), 5000);
 
 // create dummy http-server
 http
