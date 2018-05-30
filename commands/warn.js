5d3f563000435b79ce4639a83688eeee19a5df9d
Evgeny Rodionov <git@erodionov.ru>
2018-05-30 13:38:33 +0300

    feat: reasons of format

--- commands/warn.js
+++ commands/warn.js
@@ -8,16 +8,24 @@ const keywords = new Set(['формат']);
 
 const escapeMarkdown = text => text.replace(/_/g, '\\_');
 
-const getReplyText = channel => `
+const getReplyText = (channel, reasons) => `
 Привет! У нас есть [правила](https://teletype.in/@telegram-ru/r1WQe5F1m) оформления вакансий и резюме: они помогают компаниям привлекать людей, а соискателям — выбирать предложения.
 
 После этого мы постим вакансию в канал ${escapeMarkdown(channel)}. Если не отредактировать, то удалим через 5-10 минут.
+${reasons.length > 0 &&
+  `
+Нужно исправить: ${reasons.join(', ')}
+`}
 `;
 
-async function warn(msg, channel) {
-  debug('warn', msg, channel);
+async function warn(msg) {
+  const { channel } = config.get(msg.chat.username);
+  const [, ...reasons] = msg.text.split(' ');
 
-  await bot.sendMessage(msg.chat.id, getReplyText(channel), {
+  debug('warn', msg, channel);
+  debug('warn:reasons', reasons);
+
+  await bot.sendMessage(msg.chat.id, getReplyText(channel, reasons), {
     reply_to_message_id: msg.reply_to_message.message_id,
     parse_mode: 'Markdown',
   });
@@ -28,9 +36,7 @@ async function warn(msg, channel) {
 async function handler(msg) {
   try {
     if (isReply(msg) && isKeyword(msg, keywords) && isChatAdmin(msg)) {
-      const { channel } = config.get(msg.chat.username);
-
-      await warn(msg, channel);
+      await warn(msg);
     }
   } catch (err) {
     debug('handler:error', err, msg);
